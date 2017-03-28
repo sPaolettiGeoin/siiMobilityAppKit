b4f934e3a4434360d33430fbb2b46be8c465f304
Geoin01\Fabio <fabio.biffoli@gmail.com>
2017-03-28 15:35:16 +0200

    Sviluppi

--- www/js/modules/healthCare/HealthCare.js
+++ www/js/modules/healthCare/HealthCare.js
@@ -16,11 +16,8 @@ var HealthCare = {
 	},
 	open: false,
     expanded: false,
-    results: [],
-    showResult: false,
     varName: "HealthCare",
     idMenu: "healthCareMenu",
-    coordinates: "",
     menuHeaderTitle: "",
 	fieldset_characteristics: "",
 	fs_char_sex: "",
@@ -29,23 +26,6 @@ var HealthCare = {
 	hints: "",
 	hint: "",
 	healthOptions: [],
-    pathPoints: [ ],
-    lastSelected: null,
-    curDestPointQuery: 1,
-    curStartDateTime: null,
-    curMarker: null,
-
-    startPointIcon: null,
-    destPointIcon: null,
-	keyPressEnter: function (event, name) {
-        if (event.which == 13 || event.keyCode == 13) {
-            // Call Text Search Query
-            console.log("Key Press Enter! Point " + name);
-            HealthCare.searchText();
-            return false;
-        }
-        return true;
-    },
 	init: function() {
         HealthCare.menuHeaderTitle = Globalization.labels.healthCareMenu.title;
 		HealthCare.fieldset_characteristics = Globalization.labels.healthCareMenu.characteristics;
@@ -58,48 +38,12 @@ var HealthCare = {
 		HealthCare.modifyButton = Globalization.labels.healthCareMenu.modifyButton;
 		HealthCare.hints = Globalization.labels.healthCareMenu.hints;
 		HealthCare.healthAction = Globalization.labels.healthCareMenu.healthAction;
-        //HealthCare.startPointIcon = Utility.checkServiceIcon(RelativePath.images + SettingsManager.language + '/FirstStopOfRoute.png', "classic");
-        //HealthCare.destPointIcon = Utility.checkServiceIcon(RelativePath.images + SettingsManager.language + '/LastStopOfRoute.png', "classic");
         
 		HealthCare.loadDatiAnagrafici();
         HealthCare.show();
 		
 		HealthCare.getHint();
 		HealthCare.getHealthOptions();
-        //GpsManager.stopWatchingPosition();
-        //MapManager.removeAndUpdatePopUpGpsMarker();
-		/*
-        MapManager.map.on('singleclick', function (event) {
-            if (HealthCare.open) {
-                if (MapManager.map != null) {
-                    if (MapManager.manualMarker != null) {
-                        $(document.getElementById('manualPopup')).popover('destroy');
-                        var element = document.getElementById('manualPopup');
-                        if (MapManager.manualMarkerPopUp == null) {
-                            MapManager.manualMarkerPopUp = new ol.Overlay({
-                                element: element,
-                                offset: [0, -23],
-                                positioning: 'bottom-center'
-                            });
-
-                            MapManager.map.addOverlay(MapManager.manualMarkerPopUp);
-                        }
-
-                        MapManager.manualMarkerPopUp.setPosition(MapManager.manualMarker.getSource().getFeatures()[0].getGeometry().getCoordinates());
-                        if (!HealthCare.showResult && HealthCare.lastSelected && (HealthCare.lastSelected.type == "point")) {
-                            $(element).popover({
-                                'placement': 'top',
-                                'animation': false,
-                                'html': true,
-                                'content': "<h4><a onclick=\"HealthCare.setCoordinates();HealthCare.searchLocation();MapManager.closePopUp();\" style=\"text-decoration: none;cursor: pointer;\"><b style=\"color: blue; white-space: nowrap;\"> " + Globalization.labels.healthCareMenu.popupSelectLabel + " </b></a></h4><h4><a onclick=\"MapManager.removeManualMarker();\" style=\"text-decoration: none;cursor: pointer;\"><b style=\"color: red\"> " + Globalization.labels.healthCareMenu.popupCancelLabel + " </b></a></h4>"
-                            });
-                            $(element).popover('show');
-                        }
-                    }
-                }
-            }
-        });
-		*/
     },
 	show: function () {
         application.resetInterface();
@@ -169,76 +113,10 @@ var HealthCare = {
         HealthCare.expanded = false;
     },
 
-    searchLocation: function() {
-		//console.log("dbg040");
-        var locationQuery = QueryManager.createLocationQuery(HealthCare.coordinates, "user");
-        APIClient.executeQuery(locationQuery, HealthCare.successQueryLocation, HealthCare.errorQuery);
-    },
-
-    searchText: function() {
-		//console.log("dbg050");
-        var text = $("#textSearch" + HealthCare.lastSelected.name).val();
-        var textQuery = QueryManager.createFullTextQuery(text);
-        APIClient.executeQuery(textQuery, HealthCare.successQueryText, HealthCare.errorQuery, "user");
-    },
-    renderSingleService: function(singleService){
-		//console.log("dbg010: " + singleService);
-        ViewManager.render(singleService, "#" + InfoManager.idMenu, "js/modules/HealthCare.mst.html");
-    },
-//callBack
-    successQueryLocation: function (response) {
-		//console.log("dbg030: " + response);
-        for (var point of PathFinder.pathPoints) {
-            if (PathFinder.lastSelected && (PathFinder.lastSelected.name == point.name)) {
-                point.coordinates = PathFinder.coordinates;
-                point.address = response.address + ", " + response.number + " " + response.municipality;
-                ViewManager.render(point, "#pathFinderPoint" + point.name, "PathFinderPoint");
-                $("#select" + point.name).val(point.type);
-                PathFinder.addPointMarker(point);
-                break;
-            }
-        }
-    },
-
-    successQueryPath: function (response) {
-		//console.log("dbg070");
-        if (response.journey && response.journey.routes[0]) {
-            var i = PathFinder.curDestPointQuery;
-            var indexRoute = 0;
-            for (var r of response.journey.routes) {
-                PathFinder.curStartDateTime = r.eta;
-                for (var indexArc in r.arc) {
-                    response.journey.routes[indexRoute].arc[indexArc].index = i + "_" + indexRoute + "_" + indexArc;
-                }
-                response.journey.routes[indexRoute].index = i+ "_" + indexRoute;
-                indexRoute++;
-            }
-            PathFinder.results.push({  index: i,
-                            sourcePoint: PathFinder.pathPoints[i-1],
-                            destPoint: PathFinder.pathPoints[i],
-                            journey: response.journey});
-        }
-        PathFinder.curDestPointQuery++;
-        PathFinder.getSubPath();
-    },
-
-    successQueryText: function (response) {
-		//console.log("dbg080");
-        for (var f of response.features) {
-            //console.log(f.id + " " + f.properties.name + " " + f.properties.tipo)
-        }
-    },
-
     //callBack
     errorQuery: function(error) {
 		console.log("dbg090: " + JSON.stringify(error));
         navigator.notification.alert(Globalization.alerts.servicesServerError.message, function() {}, Globalization.alerts.servicesServerError.title);
-    },
-
-    resetSearch: function () {
-		//console.log("dbg100");
-        QueryManager.resetMaxDists();
-        Loading.hideAutoSearchLoading();
     },
 	loadDatiAnagrafici: function() {
 		HealthCare.datiAnagrafici.name = localStorage.getItem("name");
@@ -264,19 +142,22 @@ var HealthCare = {
 		HealthCare.switchUpdateDatiAnagrafici(false);
 	},
 	getHint: function() {
-        //var actionQuery = QueryManager.createRetrieveActionsQuery();
-		var actionQuery = "getHint";
+        var actionQuery = "getHint";
 		appoAPIClient.executeQuery(actionQuery, HealthCare.successQueryAction, HealthCare.errorQuery);
     },
 	getHealthOptions: function() {
-        //var actionQuery = QueryManager.createRetrieveActionsQuery();
-		var actionQuery = "getHealthOptions";
+        var actionQuery = "getHealthOptions";
 		appoAPIClient.executeQuery(actionQuery, HealthCare.successQueryAction, HealthCare.errorQuery);
     },
 	loadHealthAction: function() {
-		HealthCare.healthHint = $(this).find('option:selected').val();
-		//$(this).find('option:selected').val();
-		HealthCare.refreshMenu();
+		var selectedOption = $("#healthActions option:selected");
+		for (var indx = 0; indx < HealthCare.healthOptions.length; indx++) {
+			HealthCare.healthOptions[indx].selected = HealthCare.healthOptions[indx].key === selectedOption.val() ? "selected" : "";
+		}
+		var actionQuery = "getHealthGoal?goal=" + selectedOption.val();
+		//console.log("actionQuery: " + actionQuery);
+		appoAPIClient.executeQuery(actionQuery, HealthCare.successQueryAction, HealthCare.errorQuery);
+		//HealthCare.refreshMenu();
 	},
 	successQueryAction: function (actionQuery, response) {
 		if (actionQuery === "getHint") {
@@ -285,6 +166,10 @@ var HealthCare = {
 		else if (actionQuery === "getHealthOptions") {
 			HealthCare.healthOptions = response;
 		}
+		else if (actionQuery === "getHealthGoal") {
+			HealthCare.healthHint = response;
+		}
+		
 		HealthCare.refreshMenu();
     },
 }
@@ -296,6 +181,8 @@ var appoAPIClient = {
 					{key: "walk_more", value: "Camminare di più"}
 					],
 	executeQuery: function(actionQuery, successQueryAction, errorQuery) {
+		//console.log("actionQuery: " + actionQuery);
+		//console.log("actionQuery.split('?')[0]: " + actionQuery.split("?")[0]);
 		if (actionQuery === "getHint") {
 			var randomHintIndex = Math.floor((Math.random() * this.hints.length));
 			successQueryAction(actionQuery, this.hints[randomHintIndex]);
@@ -303,8 +190,38 @@ var appoAPIClient = {
 		else if (actionQuery === "getHealthOptions") {
 			successQueryAction(actionQuery, this.healthOptions);
 		}
+		else if (actionQuery.split("?")[0] === "getHealthGoal") {
+			var healthGoal = appoAPIClient.getParameterFromPath(actionQuery, "goal");
+			//console.log("healthGoal: " + healthGoal);
+			if (healthGoal === "loss_weigth") {
+				successQueryAction(actionQuery.split("?")[0], "Porta fuori il cane");
+			}
+			else if (healthGoal === "walk_more") {
+				successQueryAction(actionQuery.split("?")[0], "Invece di prendere l'auto per andare a lavoro, prendi l'autobus a soli 800 metri da casa. La linea 6 ti porta a 500 metri dal lavoro e camminerai per 20 minuti al giorno.");
+			}
+			else {
+				errorQuery("Unknown healthGoal");
+			}
+		}
 		else {
 			errorQuery("Unknown action");
 		}
+	},
+	getParameterFromPath: function(url, paramName) {
+		var value = null;
+		if (url != null && url.indexOf("?") > -1) {
+			var queryString = url.split("?")[1];
+			var paramsPairs = queryString.split("&");
+			var paramsPair;
+			for (var indx = 0; indx < paramsPairs.length; indx++) {
+				if (paramsPairs[indx].indexOf("=") > -1) {
+					paramsPair = paramsPairs[indx].split("=");
+					if (paramsPair[0] == paramName) {
+						value = paramsPair[1];
+					}
+				}
+			}
+		}
+		return value;
 	}
 }
